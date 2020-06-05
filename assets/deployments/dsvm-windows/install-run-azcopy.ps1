1a4f82a30a4af170d95243bad3e921bd485bf158
Dave Baskin <dfbaskin@gmail.com>
2020-06-05 15:56:45 -0400

    Adding copyright headers.

--- assets/deployments/dsvm-windows/install-run-azcopy.ps1
+++ assets/deployments/dsvm-windows/install-run-azcopy.ps1
@@ -1,3 +1,6 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
 # Installs AzCopy and copies dataset from Azure Storage.
 
 Param (
--- src/scripts/build-batch-applications.ps1
+++ src/scripts/build-batch-applications.ps1
@@ -1,3 +1,6 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
 
 $root = Join-Path $PSScriptRoot ".."
 $buildPath = Join-Path $PSScriptRoot "build"
